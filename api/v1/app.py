6f38b81b6088c1591cd732413c86dff18f94252f
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-04-05 12:48:05 +0300

    api

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" imported modules """
+from flask import Flask, Blueprint, abort
+from flask import make_response
+from flask import render_template, jsonify
+from app.v1.views import app_views
+from flask_cors import CORS
+import os
+from models import storage
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.errorhandler(404)
+def invalid_route(e):
+    return (jsonify({"error": "Not found"}), 404)
+
+
+@app.teardown_appcontext
+def storage_close(issue):
+    """calls storage.close()"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')),
+            threaded=True, debug=True)
+
+
+    
