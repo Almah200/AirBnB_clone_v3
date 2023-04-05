b070a52b7ccd96c79ad516220088166f9a32f472
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2023-04-05 15:48:04 +0300

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 """ Create a folder views inside"""
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def _status():
-    """returns a JSON file with Status: OK"""
-    return jsonify(status= "OK")
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """returns a JSON file"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     """retrieves the number of each objects by type"""
-    return jsonify(amenities = storage.count("Amenity"),
-                   cities = storage.count("City"),
-                   places = storage.count("Place"),
-                   reviews = storage.count("Review"),
-                   states = storage.count("State"),
-                   users = storage.count("User"))
+    result ={
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(result)
